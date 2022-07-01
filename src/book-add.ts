2509aab24f498b5f63098686585ae6ffec8fa883
Andriannus Parasian <andriannus.parasian@efishery.com>
2022-07-01 14:05:15 +0700

    feat: add toggle button for `isComplete` value

--- src/book-add.ts
+++ src/book-add.ts
@@ -1,5 +1,32 @@
 import PrevIcon from "./assets/icons/previous.png";
 
+const BUTTON_DEACTIVE_CLASS = "Button--outlined";
+
+const payload: Record<string, string | boolean | number | null> = {
+  isComplete: null,
+};
+
+export function bookAddMounted(): void {
+  const buttons = document.querySelectorAll<HTMLButtonElement>(
+    ".ButtonGroup .Button",
+  );
+
+  if (buttons.length < 1) return;
+
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      const value = parseInt(button.getAttribute("data-value") ?? "");
+      payload.isComplete = value;
+
+      for (let i = 0; i < buttons.length; i += 1) {
+        buttons[i].classList.add(BUTTON_DEACTIVE_CLASS);
+      }
+
+      buttons[index].classList.remove(BUTTON_DEACTIVE_CLASS);
+    });
+  });
+}
+
 export const BookAdd = /*html*/ `
 <header class="AppBar">
   <a
@@ -34,8 +61,34 @@ export const BookAdd = /*html*/ `
 
         <text-field
           id="TxtYearOfPublication"
+          class="mb-md"
           placeholder="Tahun terbit"
         ></text-field>
+
+        <div class="ButtonGroup mb-md" role="group">
+          <button
+            data-value="0"
+            class="Button Button--success Button--outlined Button--fullWidth"
+            type="button"
+          >
+            Sedang dibaca
+          </button>
+
+          <button
+            data-value="1"
+            class="Button Button--success Button--outlined Button--fullWidth"
+            type="button"
+          >
+            Selesai dibaca
+          </button>
+        </div>
+        
+        <button
+          class="Button Button--fullWidth Button--primary"
+          type="button"
+        >
+          Simpan
+        </button>
       </form>
     </div>
   </div>
