48332f5183a3ddf7e1fb8fa30dae35ecad46aaeb
Andriannus Parasian <andriannus.parasian@efishery.com>
2022-07-04 12:22:36 +0700

    refactor: adjust `main.ts` file

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,10 @@
-import { routes } from "./routes";
+import { useRouter } from "./utils/router";
+
 import "./components/search-field";
 import "./components/text-field";
 import "./style.scss";
 
-interface NavigateParams {
-  event: Event;
-  pathname: string;
-  replace?: boolean;
-}
+const router = useRouter();
 
-function handleNavigate({
-  event,
-  pathname,
-  replace = false,
-}: NavigateParams): void {
-  event.preventDefault();
-
-  const url = window.location.origin + pathname;
-
-  if (replace) {
-    window.history.replaceState({}, pathname, url);
-  } else {
-    window.history.pushState({}, pathname, url);
-  }
-
-  showCurrentPage();
-}
-
-function handleAnchorElements(): void {
-  const selectors = [".Link", ".AppBar-backButton"];
-
-  selectors.forEach((selector) => {
-    const anchorElements = document.querySelectorAll<HTMLAnchorElement>(
-      `${selector}`,
-    );
-
-    if (!anchorElements.length) return;
-
-    anchorElements.forEach((anchorElement) => {
-      const href = anchorElement.getAttribute("href") ?? "/";
-
-      anchorElement.addEventListener("click", (event: Event) => {
-        event.preventDefault();
-
-        handleNavigate({
-          event,
-          pathname: href,
-          replace: selector === ".AppBar-backButton",
-        });
-      });
-    });
-  });
-}
-
-const app = document.querySelector<HTMLDivElement>("#app");
-
-function showCurrentPage(): void {
-  if (!app) return;
-
-  const { component, onMount } = routes[window.location.pathname];
-
-  app.innerHTML = `
-    <div class="DefaultLayout">${component}</div>
-  `;
-
-  onMount && onMount();
-  handleAnchorElements();
-}
-
-window.onpopstate = showCurrentPage;
-window.onload = showCurrentPage;
+window.onpopstate = router.show;
+window.onload = router.show;
